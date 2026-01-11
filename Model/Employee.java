845dfbc0b4795d2414693f62f6386aa20502ad5f
Shathees1234 <shatheesg833@gmail.com>
2026-01-11 12:47:59 +0530

    Add Employee class with leave management

--- Model/Employee.java
+++ Model/Employee.java
@@ -1,1 +1,23 @@
 
+package com.smartops.model;
+
+public class Employee {
+
+    private int empId;
+    private String name;
+    private String department;
+    private int totalLeave;
+    private int usedLeave;
+
+    public Employee(int empId, String name, String department) {
+        this.empId = empId;
+        this.name = name;
+        this.department = department;
+        this.totalLeave = 20;
+        this.usedLeave = 0;
+    }
+
+    public int getRemainingLeave() {
+        return totalLeave - usedLeave;
+    }
+}
